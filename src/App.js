2f9e269b1ab836094e2b3a5aea3dce4415f1180d
Tobias Quante <t.quante@outlook.com>
2021-08-02 18:58:51 +0200

    feat: add fetch for github data

--- src/App.js
+++ src/App.js
@@ -4,13 +4,32 @@ import Users from './components/Users/Users';
 import './App.css';
 
 class App extends Component {
+  state = {
+    users: [],
+    loading: false
+  }
 
+  async componentDidMount() {
+    try {
+      this.setState({ loading: true });
+      const res = await fetch('https://api.github.com/users')
+      const data = await res.json();
+      this.setState({ users: data });
+    } catch (err) {
+      console.warn(err);
+    } finally {
+      this.setState({ loading: false });
+
+    }
+
+
+  }
   render() {
     return (
       <div className="App">
         <Navbar />
         <div className="container">
-          <Users />
+          <Users loading={this.state.loading} users={this.state.users} />
         </div>
       </div>
     );
