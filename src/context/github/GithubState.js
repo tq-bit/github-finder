c0a10709f5e77c268060d85b911c849b8d6870b7
Tobias Quante <t.quante@outlook.com>
2021-08-22 08:40:05 +0200

    ref: move process env to distinct vars

--- src/context/github/GithubState.js
+++ src/context/github/GithubState.js
@@ -5,6 +5,18 @@ import {
   CLEAR_USERS, GET_REPOS, GET_USER, SEARCH_USERS, SET_LOADING
 } from '../types';
 
+let githubClientId;
+let githubClientSecret;
+
+if (process.env.NODE_ENV !== 'production') {
+  githubClientId = process.env.REACT_APP_GITHUB_CLIENT_ID;
+  githubClientSecret = process.env.REACT_APP_GITHUB_CLIENT_SECRET;
+} else {
+  githubClientId = process.env.GITHUB_CLIENT_ID;
+  githubClientSecret = process.env.GITHUB_CLIENT_SECRET;
+
+}
+
 const GithubState = props => {
   const initialState = {
     users: [],
@@ -20,7 +32,7 @@ const GithubState = props => {
     try {
       setLoading();
       const res = await fetch(
-        `https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_ID}`
+        `https://api.github.com/search/users?q=${text}&client_id=${githubClientId}&client_secret=${githubClientSecret}`
       );
       const data = await res.json();
       dispatch({ type: SEARCH_USERS, payload: data.items })
@@ -33,7 +45,7 @@ const GithubState = props => {
     try {
       setLoading();
       const res = await fetch(
-        `https://api.github.com/users/${username}?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_ID}`
+        `https://api.github.com/users/${username}?client_id=${githubClientId}&client_secret=${githubClientSecret}`
       );
       const data = await res.json();
       dispatch({ type: GET_USER, payload: data })
@@ -47,7 +59,7 @@ const GithubState = props => {
     try {
       setLoading();
       const res = await fetch(
-        `https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_ID}`
+        `https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${githubClientId}&client_secret=${githubClientSecret}`
       );
       const data = await res.json();
       dispatch({ type: GET_REPOS, payload: data })
